9cb80cf1673627bb133e38553b97b1e777aa30ba
Yamaha200x <yamaha200xxxx@gmail.com>
2025-12-17 12:36:14 +0100

    Fix Schriftart

--- Audio-Hub/Audio-Hub/MauiProgramExtensions.cs
+++ Audio-Hub/Audio-Hub/MauiProgramExtensions.cs
@@ -13,7 +13,7 @@ public static class MauiProgramExtensions
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("CenturyGothic.ttf", "CenturyGothic");
 			});
 
 #if DEBUG
