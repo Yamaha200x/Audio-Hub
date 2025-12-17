8b585be08f8f645d4550f9e54ee1570067cd02b3
Yamaha200x <yamaha200xxxx@gmail.com>
2025-12-17 12:33:11 +0100

    Fix

--- Audio-Hub/Audio-Hub/MauiProgramExtensions.cs
+++ Audio-Hub/Audio-Hub/MauiProgramExtensions.cs
@@ -6,15 +6,15 @@ namespace Audio_Hub;
 
 public static class MauiProgramExtensions
 {
-    public static MauiAppBuilder UseSharedMauiApp(this MauiAppBuilder builder)
-    {
-        builder
-.UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+	public static MauiAppBuilder UseSharedMauiApp(this MauiAppBuilder builder)
+	{
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
 
 #if DEBUG
         builder.Logging.AddDebug();
